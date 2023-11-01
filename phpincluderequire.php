1d385f64bf918e0d40029dcefd2a1505f688b72c
Glenn Brennand <glennbrennand@hotmail.com>
2023-11-01 14:01:27 +0100

    phpincluderequire 3.2 (#3)

---
+++ phpincluderequire.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<div class="menu">
+<?php include 'menu.php';?>
+</div>
+
+<h1>welkom</h1>
+<p>paragraaf 1</p>
+<p>paragraaf 2</p>
+<?php include 'footer.php';?>
+
+</body>
+</html>
