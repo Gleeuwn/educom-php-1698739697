006ade6733accaa1f0308496381b1c3dd54ec3b4
Glenn Brennand <glennbrennand@hotmail.com>
2023-11-01 09:34:18 +0100

    phpsyntax 1.1 (#1)

---
+++ phpsyntax.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>mijn eerste php-pagina</h1>
+
+<?php
+echo "Hello World!"<br>;
+EchO "Hello World!"<br>;
+eCHO "Hello World!"<br>;
+
+$color = "red"
+echo "My car is" . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "my boat is " . $coLOR . "<br>";
+?>
+
+</body>
+</html>
